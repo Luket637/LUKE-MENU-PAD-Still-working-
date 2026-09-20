a9a3eb3c1db54674b76ccd42eaa09101ee8c3ddc
Luket637 <lucasrabello58@gmail.com>
2026-09-20 08:33:35 -0300

    Add PluginInfo class for LRMenuPad

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LRMenuPad
+{
+    public static class PluginInfo
+    {
+        public const string GUID = "com.lrmenupad.unr1";
+        public const string Name = "LR MENU PAD";
+        public const string Version = "UNR 1";
+    }
+}
